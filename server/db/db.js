8c77007ff81440cf7449091f5624f73fe75b8fbb
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-06 14:22:09 +0000

    connects to remote db instance

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,8 @@
 const path = require('path');
 const chalk = require('chalk');
 const Sequelize = require('sequelize');
-const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+//const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+const DATABASE_URI = 'postgres://10.0.11.99/juke';
 
 console.log(chalk.yellow('Opening connection to PostgreSQL'));
 
