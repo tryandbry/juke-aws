afc8f3d3a3d8623d1660803c8cf2e46526024491
Bryan Kao <bryan.w.kao@gmail.com>
2017-06-08 04:35:46 +0000

    connects to AWS RDS (postgresql)

--- server/db/db.js
+++ server/db/db.js
@@ -3,19 +3,18 @@ const path = require('path');
 const chalk = require('chalk');
 const Sequelize = require('sequelize');
 //const DATABASE_URI = 'postgres://10.0.11.99/juke';
-const DATABASE_URI = 'mysql01.czjyxj8uh93i.us-east-2.rds.amazonaws.com';
-const {DB, USER, PW} = require('../../../juke.secrets');
+const {DB, USER, PW, URI} = require('../../../juke.secrets');
 
-console.log(chalk.yellow('Opening connection to PostgreSQL'));
+console.log(chalk.yellow('Opening connection to AWS database'));
+
+const endpoint = 'postgres://' + USER + ":" + PW + '@' + URI + DB;
 
 // create the database instance
-/*
-module.exports = new Sequelize(DATABASE_URI, {
+module.exports = new Sequelize(endpoint, {
   logging: false, // set to console.log to see the raw SQL queries
   native: true // lets Sequelize know we can use pg-native for ~30% more speed
 });
-*/
-
+/*
 module.exports = new Sequelize(DB,USER,PW,{
   logging: false,
   host: DATABASE_URI,
@@ -28,3 +27,4 @@ module.exports = new Sequelize(DB,USER,PW,{
     idle: 10000
   },
 });
+*/
